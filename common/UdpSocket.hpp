e68653ae6f466605f667a07c9e584cc35805b450
Roberatorul <robertconturi360@gmail.com>
2026-04-19 22:07:37 +0300

    add comments

--- common/UdpSocket.hpp
+++ common/UdpSocket.hpp
@@ -11,12 +11,20 @@ private:
 	int sockfd;
 
 public:
-	UdpSocket();
-	~UdpSocket();
+	UdpSocket(); // Creates an IPv4 socket
+	~UdpSocket(); // Closes the file descriptor
 
-	int getFd() const;
+	int getFd() const; // Returns socket file descriptor
 
+	/* Binds the socket to a specific port */
 	void bindToPort(int port, const std::string& ip_address = "0.0.0.0");
+
+	/* Sends the pkt packet to a destination dest_addr */
 	void sendPacket(const Packet& pkt, const struct sockaddr_in& dest_addr);
+
+	/* 
+	* Receives a packet into pkt and also fills sender address sender_addr
+	* Return value is the one from recvfrom(), letting user decide how to handle errors
+	*/
 	ssize_t recvPacket(Packet& pkt, struct sockaddr_in& sender_addr);
 };
--- common/utils.hpp
+++ common/utils.hpp
@@ -16,8 +16,8 @@ enum class PacketType : uint8_t {
 struct Packet {
 	PacketType type;
 	uint32_t seq_num; // Used for Go-Back-N
-	uint32_t len;
-	char payload[MAX_PAYLOAD_LEN];
+	uint32_t len; // Number of valid bytes in the payload
+	char payload[MAX_PAYLOAD_LEN]; // Actual data buffer
 
 	Packet() : type(PacketType::DATA), seq_num(0), len(0) {
 		std::fill(std::begin(payload), std::end(payload), 0);
