6c0ed6988dbace3e6036d7302da41d8275521799
Roberatorul <robertconturi360@gmail.com>
2026-04-24 12:13:37 +0300

    Fix fill_packet() length bug

--- common/utils.cpp
+++ common/utils.cpp
@@ -7,11 +7,11 @@ Packet::Packet() : type(PacketType::DATA), seq_num(0), len(0) {
 void Packet::fill_packet(PacketType t, uint32_t seq, uint32_t l, 
 						const char* data) {
 	this->type = t;
-	this->seq_num = seq_num;
-	this->len = len;
+	this->seq_num = seq;
+	this->len = l;
 
 	std::fill(std::begin(this->payload), std::end(this->payload), 0);
 
 	if (data != nullptr && l > 0)
-		memcpy(this->payload, payload, sizeof(this->payload));
+		memcpy(this->payload, data, sizeof(this->payload));
 }
